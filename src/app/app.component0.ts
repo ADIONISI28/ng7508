9c643dfbca2f778e8faeb28be8865e9212785916
adionisi28 <adionisi@gmail.com>
2018-03-06 20:03:04 -0300

    Aula1

--- src/app/app.component0.ts
+++ src/app/app.component0.ts
@@ -4,29 +4,9 @@ import { Component } from '@angular/core'
   selector: 'app-root',
   //template: `<h1> Alberto is the best 4<h1>`,
   templateUrl: './app.component.html',  // se fosse / ia ser /src com ./ vai na propria pasta ./app
-  
-   styles:  [`
-  header
-  {
-    width: 100%;
-    text-align: center;
-  }
-  header h1
-  {
-    font-size:24px; 
-    text-transform: uppercase; 
-    letter-spacing:10px; 
-    color:white; 
-  }
-
-  section img
-  {
-    border:4px solid red;
-  }
-  `] 
+  styles: ['h1 {color: red }']
 })
 
 export class AppComponent {
   title = "Caelum Pic";
-  creditos = "(Valeu Raphael)";
 }
